daebed015e6b6479f92a2e243516ba13a103658a
pringle97 <malia.pringle@gmail.com>
2022-01-04 16:34:58 -0800

    adding files

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../models')
+const passport = require('passport')
 
 router.post('/', async(req, res) => {
 
---
+++ seeders/postSeed.js
@@ -0,0 +1,20 @@
+const seed = [
+  {
+    content: 'testing1',
+    postId: 1
+  },
+  {
+    content: 'something2',
+    postId: 1
+  },
+  {
+    content: 'something3',
+    postId: 2
+  },
+  {
+    content: 'something4',
+    postId: 3
+  }
+]
+
+module.exports = seed
