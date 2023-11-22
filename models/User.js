944acf7968d3e3b4d2243551407839f1a46e8172
Malia Pringle <malia.pringle@gmail.com>
2023-11-21 17:57:37 -0800

    cleaning code

--- models/User.js
+++ models/User.js
@@ -2,18 +2,6 @@ const pls = require('passport-local-sequelize')
 const { Model, DataTypes } = require('sequelize')
 const sequelize = require('../db/index.js')
 
-// class User extends Model { }
-// User.init({
-//   username: {
-//     type: DataTypes.STRING,
-//     allowNull: false
-//   },
-//   email: {
-//     type: DataTypes.STRING,
-//     allowNull: false
-//   }
-// }, { sequelize, modelName: 'user' })
-
 const User = pls.defineUser(sequelize, {
   username: {
     type: DataTypes.STRING,
