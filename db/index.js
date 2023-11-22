bcf982d4203ccae9f5fd42c0134f436d22d18b75
Malia Pringle <malia.pringle@gmail.com>
2023-11-21 17:30:28 -0800

    fixing mysql port

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const sequelize = process.env.JAWSDB_URL ? new Sequelize(process.env.JAWSDB_URL): new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: 'localhost',
-  port: 3000, 
+  port: 3306, 
   dialect: 'mysql'
 })
 
