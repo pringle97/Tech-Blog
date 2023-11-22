dbdcddf0513ca1cdc30637ebd2eb219806a7e21a
Malia Pringle <malia.pringle@gmail.com>
2023-11-21 17:39:36 -0800

    fixing port error

--- server.js
+++ server.js
@@ -44,6 +44,9 @@ app.listen(process.env.PORT || 3000, function () {
 
 async function init() {
   await require('./db').sync()
+  app.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  })
   };
 
 init()
