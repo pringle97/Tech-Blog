67d1fedabc82ce4091826a2e006c12008642bb74
Malia Pringle <malia.pringle@gmail.com>
2023-11-28 13:27:35 -0800

    changing function names

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -2,22 +2,20 @@ const router = require('express').Router()
 const passport = require('passport')
 const { Post, User, Comment } = require('../models')
 
+// get comments
 router.get('/comments', passport.authenticate('jwt'), async function (req, res) {
   const comments = await Comment.findAll({ include: [User, Post] })
   res.json(comments)
+  console.log(comments)
 })
 
-router.get('/comments/:id', passport.authenticate('jwt'), async function (req, res) {
-  const comments = await Comment.findAll({ where: { pid: req.params.id }, include: [User] })
-  res.json(comments)
-})
-
-router.post('/comments', passport.authenticate('jwt'), async function (req, res) {
-  console.log(req.body) 
-  const comments = await Comment.create({ 
+//post a comment
+router.post('/:id', passport.authenticate('jwt'), async function (req, res) {
+  const comments = await Comment.create({
     content: req.body.content,
-    uid: req.user.id,
-    pid: req.body.pid
+    pid: req.body.pid,
+    uid: req.session.userId,
+    username: req.session.username
   })
   res.json(comments)
 })
